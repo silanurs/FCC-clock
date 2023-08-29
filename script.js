4d7d0baf958914e97ee07a1316920342592fed1c
silanurs <sarii.silanur@gmail.com>
2023-08-29 20:42:57 +0300

    removed node modules and modified scrip.js

--- script.js
+++ script.js
@@ -1,8 +1,44 @@
-import $ from 'jquery';
+
 $(document).ready(function(){
     $(".break-decrement").click(function(){
-        let breakVal = $(".break-length").text()
-        $(".break-length").text(breakVal--)
+        let breakVal = Number($(".break-length").text())
+        let newBreak 
+        if(breakVal===0){
+            newBreak=0
+        }else {
+            newBreak=breakVal - 1;
+        }
+        $(".break-length").text(`${newBreak}`)
+    })
+    $(".break-increment").click(function(){
+        let breakVal = Number($(".break-length").text());
+        let newBreak
+        if(breakVal===60){
+            newBreak=60;
+        }else{
+            newBreak= breakVal+1;
+        }
+        $(".break-length").text(`${newBreak}`)
+    })
+    $(".session-decrement").click(function(){
+        let sessionVal = Number($(".session-length").text())
+        let newSession
+        if(sessionVal===0){
+            newSession=0;
+        }else{
+            newSession= sessionVal-1;
+        }
+        $(".session-length").text(`${newSession}`)
+    })
+    $(".session-increment").click(function(){
+        let sessionVal = Number($(".session-length").text())
+        let newSession
+        if(sessionVal===60){
+            newSession=60;
+        }else{
+            newSession= sessionVal+1;
+        }
+        $(".session-length").text(`${newSession}`)
     })
 
 })
