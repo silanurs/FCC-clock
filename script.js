d2a7b9959b307290d82f08bee2f7d517339fa5c7
silanurs <sarii.silanur@gmail.com>
2023-09-04 20:45:45 +0300

    feat: completed writing countdown functions

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     $(".break-decrement").click(function(){
         let breakVal = Number($(".break-length").text())
         let newBreak 
-        if(breakVal===0){
-            newBreak=0
+        if(breakVal===1){
+            newBreak=1
         }else {
             newBreak=breakVal - 1;
         }
@@ -17,16 +17,20 @@ $(document).ready(function(){
             newBreak=60;
         }else{
             newBreak= breakVal+1;
+
         }
         $(".break-length").text(`${newBreak}`)
     })
     $(".session-decrement").click(function(){
         let sessionVal = Number($(".session-length").text())
         let newSession
-        if(sessionVal===0){
-            newSession=0;
+        if(sessionVal===1){
+            newSession=1;
         }else{
             newSession= sessionVal-1;
+        }
+        if (newSession <10){
+            newSession = "0" + newSession;
         }
         $(".session-length").text(`${newSession}`)
         $(".time-left-minute").text(`${newSession}`)
@@ -45,8 +49,66 @@ $(document).ready(function(){
     $("#reset").click(function(){
         $(".session-length").text('25');
         $(".break-length").text("5");
-        $(".time-left-minute").text("25")
+        $(".time-left-minute").text("25");
+        $(".time-left-second").text("00")
     })
+    let sec="00"
+    let newSec
+    function setSecond(){
+        
+        if(sec=="00"|| $(".time-left-second").text()==0){
+            newSec=59;
+            sec=newSec
+        }else {
+           newSec--;
+           sec=newSec;
+           if(newSec<10){
+            newSec="0" + sec
+           }
+        }
+        $(".time-left-second").text(`${newSec}`)
+        
+        }
+ $("button").click(function(){
+    let minute = $(".time-left-minute").text();
+    let newMin
+    function setMinute(){
+        if($(".time-left-second").text()=="00"){
+            newMin=minute-1;
+            $(".time-left-minute").text(`${newMin}`)
+            if(newMin<10){
+                $(".time-left-minute").text("0"+newMin)
+            }
+        }
+        if($(".session").text()=="Session" && $(".time-left-minute").text()=="00" && $(".time-left-second").text()=="00"){
+            $(".session").text("Break");
+            let breakMin= "0" + $(".break-length").text()
+            $(".time-left-minute").text(breakMin);
+            $(".time-left-second").text("00")
+        }
+    }
+        $("button").attr("disabled", true)
+
+        
+        let secondInterval = setInterval(setSecond, 1000)
+        let minuteInterval=setInterval(setMinute, 1000)
+        
+            secondInterval
+            minuteInterval
+        $("#stop").click(function(){
+            clearInterval(secondInterval)
+            $("button").removeAttr("disabled")
+        })
+   
+  
+    })
+    if($(".session").text()=="Session" && $(".time-left-minute").text()=="00" && $(".time-left-second").text()=="00"){
+        $(".session").text("Break");
+        let breakMin= "0" + $(".break-length").text()
+        $(".time-left-minute").text(breakMin);
+        $(".time-left-second").text("00")
+    }
+   
     
 
 })
